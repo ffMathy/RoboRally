2226473762503264cde4c24b9fa247a7b9e0d847
Mads T. Carstensen <Exceed@7complex.net>
2018-02-11 18:26:58 +0100

    deck creation

--- src/RoboRally.Core/Cards/CardDeck.cs
+++ src/RoboRally.Core/Cards/CardDeck.cs
@@ -6,5 +6,38 @@ namespace RoboRally.Core.Cards
 {
     public class CardDeck : ICardDeck
     {
+		private IList<ICard> _cards;
+
+		public CardDeck(IList<ICard> cards)
+		{
+			_cards = cards;
+		}
+
+		public ICard TakeCard()
+		{
+			var card = _cards[0];
+			_cards.Remove(card);
+			return card;
+		}
+
+		public void ReturnCard(ICard card)
+		{
+			_cards.Add(card);
+		}
+
+		public void Shuffle()
+		{
+			var random = new Random();
+
+			var shuffledCards = _cards;
+			for (int n = shuffledCards.Count - 1; n > 0; --n)
+			{
+				int k = random.Next(n + 1);
+				var temp = shuffledCards[n];
+				shuffledCards[n] = shuffledCards[k];
+				shuffledCards[k] = temp;
+			}
+			_cards = shuffledCards;
+		}
     }
 }
